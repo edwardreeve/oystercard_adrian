a8a0493fe193e18bf327d5714043161bd1378378
adrem7 <adrian.raisin@gmail.com>
2019-05-20 14:12:33 +0100

    unit tests and creation of Oystercard class and balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+  it 'is a type of Oystercard' do
+    expect(subject).to be_a_kind_of(Oystercard)
+  end
+
+  it 'has a balance of 0 as default' do
+    expect(subject.balance).to eq(0)
+  end
 end
